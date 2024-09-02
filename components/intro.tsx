3c43deb2c56ebe2f1884702fb21f4785e32c3406
Rehman <rehmansajid999@gmail.com>
2024-09-02 23:46:00 +0800

    Retry image fix

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import cycleImg from "@/public/Cycling.png";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/../public/Cycling.png"
+              src={cycleImg}
               alt="Rehman portrait"
               width="250"
               height="180"
