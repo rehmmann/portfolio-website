e90dbfb58cd0802609fbfd13442b0e1567700e0b
Rehman <rehmansajid999@gmail.com>
2024-09-02 23:32:17 +0800

    fix image

--- components/intro.tsx
+++ components/intro.tsx
@@ -33,16 +33,16 @@ export default function Intro() {
             <Image
               src="/../public/Cycling.jpg"
               alt="Rehman portrait"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              width="220"
+              height="220"
+              // quality="95"
+              // priority={true}
+              className="h-25 w-25 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
+            className="absolute bottom-0 right-5 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
